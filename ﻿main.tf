aafff63a03eda9cf87c0d25be5ba29d23da2295e
Sami Islam <islam.samipkr@gmail.com>
2023-05-05 14:09:01 -0400

    Update ﻿main.tf

--- ﻿main.tf
+++ ﻿main.tf
@@ -78,3 +78,26 @@ Name =
 "tutorial_public_subnet_${count.index}"
 
 }
+// Create a group of private subnets based on the variable subnet_count.private resource "aws_subnet" "tutorial_private_subnet" {
+// count is the number of resources we want to create
+// here we are referencing the subnet_count.private variable which
+// current assigned to 2, so 2 private subnets will be created
+count
+=
+var.subnet_count.private
+// Put the subnet into the "tutorial_vpc" VPC
+vpc_id
+= aws_vpc.tutorial_vpc.id
+// We are grabbing a CIDR block from the "private_subnet_cidr_blocks" variable
+// since it is a list, we need to grab the element based on count,
+// since count is 2, the first subnet will grab the CIDR block 10.0.101.0/24 // and the second subnet will grab the CIDR block 10.0.102.0/24
+cidr block
+= var.private_subnet_cidr_blocks [count.index]
+// We are grabbing the availability zone from the data object we created earlier // Since this is a list, we are grabbing the name of the element based on count, // since count is 2, and our region is us-east-2, the first subnet should // grab us-east-2a and the second will grab us-east-2b availability_zone = data.aws_availability_zones.available.names [count.index]
+// We are tagging the subnet with a name of "tutorial_private_subnet_" and // suffixed with the count
+{
+Name =
+tags =
+}
+}
+"tutorial_private_subnet_${count.index}"
